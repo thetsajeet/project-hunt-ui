8fd5337b7241c31e84841ca4eba6270aa5400809
T S Ajeet <ajeet.tumuluru@gmail.com>
2025-10-27 00:12:36 +0530

    create users page

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Car, PlusIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 
 export default function UserPage() {
@@ -8,14 +11,69 @@ export default function UserPage() {
   return (
     <div className="mt-4 flex flex-col gap-4">
       {/* first row */}
-      <div>
-        <div>{/* profile card */}</div>
-        <div>{/* details */}</div>
+      <div className="grid grid-cols-12 gap-1 h-[250px]">
+        <div className="col-span-3 h-full">
+          <ProfileCard />
+        </div>
+        <div className="col-span-9 h-full">
+          <BioDataCard />
+        </div>
       </div>
       {/* second row */}
-      <div>
-        <div></div>
+      <div className="grid grid-cols-12 gap-1">
+        <div className="col-span-12">
+          <ProjectsBanner />
+        </div>
+        {[...Array(4)].map((val) => (
+          <div key={val} className="col-span-3">
+            <ProjectCard />
+          </div>
+        ))}
       </div>
     </div>
+  );
+}
+
+function ProfileCard() {
+  return (
+    <Card className="w-full h-full flex justify-center items-center">
+      <CardContent>profile card</CardContent>
+    </Card>
+  );
+}
+
+function BioDataCard() {
+  return (
+    <Card className="w-full h-full flex justify-center items-center">
+      <CardContent>bio data</CardContent>
+    </Card>
+  );
+}
+
+function ProjectsBanner() {
+  return (
+    <Card className="w-full h-full flex py-2">
+      <CardContent className="h-full flex items-center">
+        <span className="text-lg flex-1">Projects</span>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="cursor-pointer hover:rounded-2xl"
+          aria-label="Add project"
+        >
+          <PlusIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
+
+function ProjectCard() {
+  return (
+    <Card className="w-full h-full">
+      <CardContent className="flex justify-center items-center h-32">
+        projects card
+      </CardContent>
+    </Card>
   );
 }
